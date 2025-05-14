ffc7e2241a5421644ebe112d6975378305678fe6
RyamL1221 <ryaml1221@gmail.com>
2025-05-14 15:31:25 -0400

    added contact form functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("nQ-1JV-MqP0VX0c9_");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_ltqtezs', 'template_6mmde8r', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
